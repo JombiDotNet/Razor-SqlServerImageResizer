2f0c20666371c5e99c507dba4625ac0cc997d82b
JombiDotNet <jombidesigns@gmail.com>
2015-07-17 04:09:37 -0500

    Logins, image uploads for authenticated users. Register new users.
    Because GitHub does not want Sql Database files to be added to source
    control, this will first fine out if the database is there. If the file
    is present, it verifies the non authentication table has been entered.
    If it has not, it prompts the user to make some file renames. If the
    table is missing, it prompts the creation of a new user, and then
    creates the image file table.

---
+++ SqlServerImageResizer/App_Data/scripts/ImageFile.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [ImageFile](
+	[ImageFileId] [int] IDENTITY(1,1) NOT NULL,
+	[ImageGuid] [uniqueidentifier] NOT NULL CONSTRAINT [DF_Media_ImageFile_ImageGuid]  DEFAULT (newid()),
+	[FileExtension] [nvarchar](15) NULL,
+	[ImageFormat] [nvarchar](15) NULL,
+	[SourcePath] [nvarchar](256) NOT NULL,
+	[ImageContents] [image] NULL,
+	[FileSize] [numeric](8, 0) NOT NULL,
+	[FileSizeKb]  AS (round([FileSize]/(1024),(2))),
+	[FileSizeMb]  AS (round([FileSize]/(1048576),(2))),
+	[ImageWidth] [int] NOT NULL,
+	[ImageHeight] [int] NOT NULL,
+	[UploadDate] [datetime] NOT NULL CONSTRAINT [DF_Media_ImageFile_UploadDate]  DEFAULT (getdate()),
+	[UserId] [int] NOT NULL,
+ CONSTRAINT [PK_ImageFile] PRIMARY KEY CLUSTERED 
+(
+	[ImageFileId] ASC
+)
+)
+GO
